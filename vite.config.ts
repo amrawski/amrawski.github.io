c5ce321e0096317ce3959b1270d4d548ac4ef145
Adam M Rawski <adammrawski@gmail.com>
2026-02-08 17:04:24 -0500

    fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/amrawski.github.io/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
